06f208bcd8f2251fa8446cab9ebc2d1a44623bbd
flow-lix <3185971499@qq.com>
2020-02-20 23:31:35 +0800

    项目初始化

---
+++ src/test/java/learn/architecture/mq/ArchitectureMqApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.architecture.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
